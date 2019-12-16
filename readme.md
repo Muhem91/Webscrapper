0e5a1d64c64b41c2a1c2de03924fcea7f24b1141
duygu koese <s53480@beuth-hochschule.de>
2019-12-16 18:16:04 +0100

    Neue Version

--- readme.md
+++ readme.md
@@ -5,3 +5,4 @@ python ./scraper.py -d <username>
 ```
 erstellt für den username einen <username>.json im dist ordner, wo sein profil und eine Liste seiner bilder gespeichert wird.
 
+
--- scraper.py
+++ scraper.py
@@ -1,51 +1,63 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import getopt
+import ssl
 import sys
-
+import time
 import urllib.request
 import urllib.parse
 import urllib.error
 from bs4 import BeautifulSoup
-import ssl
 import json
-from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
-import time
+
 
 class Profile:
     def __init__(self, instaUserData):
+        self.id = instaUserData['id']
         self.fullname = instaUserData['full_name']
         self.username = instaUserData['username']
         self.profil_picture_path = instaUserData['profile_pic_url']
 
-    def toObj(self):
-        temp = {}
-        temp['username'] = self.username
-        temp['fullname'] = self.fullname
-        temp['profilPicturePath'] = self.profil_picture_path
-        return temp
+    def to_obj(self):
+        return {
+            'username': self.username,
+            'fullname': self.fullname,
+            'profilPicturePath': self.profil_picture_path
+        }
 
     def __repr__(self) -> str:
-        return json.dumps(self.toObj())
+        return json.dumps(self.to_obj())
 
-class Picture:
-    def __init__(self, title, alt_text, picture_page, picture_path):
-        self.title = title
-        self.alt_text = alt_text
-        self.picture_page = picture_page
-        self.picture_path = picture_path
 
-    def toObj(self):
-        temp = {}
-        temp['title'] = self.title
-        temp['altText'] = self.alt_text
-        temp['picturePath'] = self.picture_path
-        return temp
+class Media:
+    def __init__(self, media_node):
+        self.pictureUrl = 'https://www.instagram.com/p/%s/'
+        try:
+            self.title = media_node['edge_media_to_caption']['edges'][0]['node']['text']
+        except Exception as e:
+            self.title = "<Could not parse title>"
+
+        try:
+            self.alt_text = media_node['accessibility_caption']
+        except Exception as e:
+            self.alt_text = "<Could not parse alternate text>"
+
+        self.path = self.pictureUrl % (media_node['shortcode'])
+        self.src = media_node['display_url']
+        self.isVideo = media_node['is_video']
+
+    def to_obj(self):
+        return {
+            'title': self.title,
+            'altText': self.alt_text,
+            'mediaPath': self.path,
+            'mediaSrc': self.src,
+            'isVideo': self.isVideo
+        }
 
     def __repr__(self) -> str:
-        return json.dumps(self.toObj())
+        return json.dumps(self.to_obj())
+
 
 class InstagramImageScraper:
 
@@ -55,30 +67,26 @@ class InstagramImageScraper:
         self.ctx.verify_mode = ssl.CERT_NONE
         self.debug = False
 
-        self.hastagUrl = 'https://www.instagram.com/explore/tags/%s/'
+        self.hashtagUrl = 'https://www.instagram.com/explore/tags/%s/'
         self.profilUrl = 'https://www.instagram.com/%s/'
         self.pictureUrl = 'https://www.instagram.com/p/%s/'
+        self.graph_ql_url = 'https://www.instagram.com/graphql/query?query_hash=%s&variables=%s'
+        self.media_query_hash = 'e769aa130647d2354c40ea6a439bfc08'
 
-    def getSharedData(self, url):
-        html = urllib.request.urlopen(url, context=self.ctx).read()
+        # put to qraph_ql variables query param -> id: id of user, first:  total - 12, after: hash of 12. picture of the page
+        self.media_variables = '{"id": "%s", "first": 12, "after": "%s"}'
+
+    def get_shared_data(self, url):
+        html = urllib.request.urlopen(url, context=self.ctx).read().decode("utf-8", "ignore")
         soup = BeautifulSoup(html, 'html.parser')
         script = soup.find('script', text=lambda t:
             t.startswith('window._sharedData'))
 
         return script
 
-
-    def getHashtags(self, hashtag):
-        url = self.hastagUrl % hashtag
-        script = self.getSharedData(url)
-
-        page_json = script.text.split(' = ', 1)[1].rstrip(';')
-        data = json.loads(page_json)
-        print(data)
-
-    def getProfile(self, instagramName):
+    def get_profile(self, instagramName):
         url = self.profilUrl % instagramName
-        script = self.getSharedData(url)
+        script = self.get_shared_data(url)
         if script:
             page_json = script.text.split(' = ', 1)[1].rstrip(';')
             data = json.loads(page_json)
@@ -87,101 +95,76 @@ class InstagramImageScraper:
         else:
             print('No Data found')
 
-    def downloadProfilPicturesInFile(self, username):
+    def download_profil_pictures_in_file(self, username):
+        data = {
+            'profile': {},
+            'total': 0,
+            'media': []
+        }
+
         url = self.profilUrl % username
-        browser = webdriver.Chrome('./chromedriver.exe')
-        # open Page in browser
-        browser.get(url)
-        time.sleep(3)
+        script = self.get_shared_data(url)
+        if script:
+            page_json = script.text.split(' = ', 1)[1].rstrip(';')
+            page_data = json.loads(page_json)
+            userData = page_data['entry_data']['ProfilePage'][0]['graphql']['user']
+            profile = Profile(userData)
+            data['profile'] = profile.to_obj()
 
-        data = {}
-        data['profile'] = self.getProfile(username).toObj()
-        data['pictures'] = []
-        data['errorAt'] = []
+            timeline_media = userData['edge_owner_to_timeline_media']
+            total_media = timeline_media['count']
+            has_next_page = timeline_media['page_info']['has_next_page']
+            end_cursor = timeline_media['page_info']['end_cursor']
 
-        # to hide the login panel for guests after some scroll
-        browser.execute_script("let s = document.createElement('style');" +
-                               "s.setAttribute('type', 'text/css');" +
-                               "s.setAttribute('rel', 'stylesheet');" +
-                               "s.innerText = 'div[role=\"presentation\"] { display: none; }';" +
-                               "let head = document.querySelectorAll('head')[0];" +
-                               "head.append(s);")
+            data['total'] = total_media
 
-        # execute script to scroll down the page
-        scrollHeight = 0
-        lastPictureY = 0
-        finish = False
-        previousI = 0
-        while not finish:
-            # newLength = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);return document.body.scrollHeight;")
-            if True:
-                allPictures = browser.find_elements_by_css_selector('a[href*=\'/p/\']')
-                newPictures = list(filter(lambda item: item.location['y'] > lastPictureY, allPictures))
+            for i, media_edge in enumerate(timeline_media['edges'], start=1):
+                print('\rProcess: %d von %d' % (i, total_media), end='')
+                try:
+                    picture = Media(media_edge['node'])
+                    data['media'].append(picture.to_obj())
+                except Exception as e:
+                    print(" --> Fehler aufgetreten.")
 
-                total = previousI + len(newPictures)
-                for i, link in enumerate(newPictures, start=1):
-                    print('\rProcess: %d von %d' % (previousI + i, total), end='')
+            while has_next_page:
+                try:
+                    # warte eine sekunde um kein 429 zu erhalten
+                    # time.sleep(1)
+                    new_variables = urllib.parse.quote_plus(self.media_variables % (profile.id, end_cursor))
+                    new_url = self.graph_ql_url % (self.media_query_hash, new_variables)
+                    print('\nLoading next page ...')
+                    media_response = urllib.request.urlopen(new_url, context=self.ctx).read().decode("utf-8")
+                    media_json = json.loads(media_response)
 
-                    # if there is a open dialog, close it again
-                    dialog = browser.find_elements_by_css_selector('div[role="dialog"]')
-                    if dialog:
-                        ActionChains(browser).send_keys(Keys.ESCAPE).perform()
+                    has_next_page =  media_json['data']['user']['edge_owner_to_timeline_media']['page_info']['has_next_page']
+                    end_cursor =  media_json['data']['user']['edge_owner_to_timeline_media']['page_info']['end_cursor']
 
-                    # scroll to the image to click on it and open the dialog
-                    browser.execute_script('window.scrollTo(%d, %d);' % (link.location['x'], link.location['y'] - 200))
-
-                    try:
-                        # link to the page with this picture and full comments
-                        picture_page = link.get_attribute('href')
-
-                        img = link.find_elements_by_tag_name('img')
-                        alt = ''
-                        picture_src = ''
-                        if img:
-                            alt = img[0].get_attribute('alt')
-                            picture_src = img[0].get_attribute('src')
-                        link.click()
-                        time.sleep(1)  # just wait a second
-
-                        # get Titel of image --> first comment in picture
-                        dialog = browser.find_elements_by_css_selector('div[role="dialog"]')[0]
-                        firstItem = dialog.find_elements_by_css_selector('ul li[role="menuitem"]')[0]
-                        title = firstItem.find_elements_by_css_selector('div > div:last-child > span')[0].text
-
-                        # close dialog
-                        ActionChains(browser).send_keys(Keys.ESCAPE).perform()
-                        data['pictures'].append(Picture(title, alt, picture_page, picture_src).toObj())
-                        time.sleep(1)  # Just wait a moment again to close the dialog
-
-                        lastPictureY = link.location['y']
-                    except Exception as e:
-                        href = link.get_attribute('href')
-                        #print(
-                        #    'Es ist ein Fehler aufgetreten bei %s. Schleife wird mit nächstem Bild fortgesetzt.' % href)
-                        data['errorAt'].append(href)
-                        browser.save_screenshot('./selenium/browser_error_picture-%d.png' % i)
-                        eingabe = input('\nEs ist ein Fehler aufgetreten. Weitermachen? [j/n]')
-                        if eingabe in ['j', 'J', 'ja', 'Ja']:
-                            continue
-                        else:
-                            finish = True
-                            break
-                print()
-                previousI = total
-                if self.debug:
-                    break
-            else:
-                print('Seite fertig geladen. Json wird erstellt....')
-                break
+                    edges = media_json['data']['user']['edge_owner_to_timeline_media']['edges']
+                    current_size = len(data['media'])
+                    for i, media_edge in enumerate(edges, start=1):
+                        print('\rProcess: %d von %d' % (current_size + i, total_media), end='')
+                        try:
+                            picture = Media(media_edge['node'])
+                            data['media'].append(picture.to_obj())
+                        except Exception as e:
+                            print(" --> Fehler aufgetreten.")
+                except Exception as ee:
+                    # print(ee)
+                    # eingabe = input('Es ist ein Fehler aufgetreten. Nochmal versuchen (und 10 sekunden warten) (y,n)')
+                    # if eingabe not in ['y', 'Y']:
+                    #     has_next_page = False
+                    # else :
+                    #    time.sleep(10)
+                    has_next_page = False
+        else:
+            print('No Data found')
 
         print()
-        browser.quit()
         filename = './dist/%s.json' % username
 
         with open(filename, 'w') as outfile:
             json.dump(data, outfile, indent=4)
         print('%s erstellt.' % filename)
-
 
 
 def main(argv):
@@ -198,27 +181,24 @@ def main(argv):
 
     if len(args) + len(opts) == 0:
         print('Fehlerhafter Aufruf. Tippe -h für Hilfe:')
-        print('scraper.py -h')
+        print('scraper_version1.py -h')
         sys.exit(1)
 
     for opt, arg in opts:
         if opt == '-h':
             print('Wenn Sie das Profil eines Benutzers sehen wollen, dann geben Sie -p <username> ein.')
             print('Um alle Bilder eines Profils in eine json zu speichern, geben Sie -d <username> ein.')
-            #print('Um alle Bilder zu einem bestimmten Hashtag in eine json zu speichern, geben Sie -t <hashtag> ein.')
-            print('z.B. scraper.py -d <username>')
+            print('z.B. scraper_version1.py -d <username>')
             sys.exit()
         elif opt in ('-p', '--profile'):
             username = arg
-            profile = scraper.getProfile(username)
+            profile = scraper.get_profile(username)
             print(profile)
         elif opt in ('-d', '--download'):
             username = arg
-            scraper.downloadProfilPicturesInFile(username)
-        elif opt in ('-t', '--hashtag'):
-            hashtag = arg
-            scraper.getHashtags(hashtag)
+            scraper.download_profil_pictures_in_file(username)
 
 
 if __name__ == "__main__":
     main(sys.argv[1:])
+
